0ca9969300f592830fc8fa82a55ea3e08bc0bc7e
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-05-11 16:38:21 +0100

    Adder complete with styling problem

--- test_turing_adder.py
+++ test_turing_adder.py
@@ -16,7 +16,16 @@ adder = TuringMachine(
         #The value will be the triple (next state, character to write, move head L or R)
         #such as ('q0', '1'): ('q1', '0', 'R'), which says if current state is q0 and 1 encountered
         #then transition to state q1, write a 0 and move head right.
-        
+        ('q0', '1'): ('q1', 'x', 'R'),
+        ('q1', '1'): ('q1', '1', 'R'),
+        ('q1', '0'): ('q2', '0', 'R'),
+        ('q2', '1'): ('q2', '1', 'R'),
+        ('q2', ''): ('q3', '1', 'L'),
+        ('q3', '1'): ('q3', '1', 'L'),
+        ('q3', '0'): ('q4', '0', 'L'),
+        ('q4', '1'): ('q4', '1', 'L'),
+        ('q4', 'x'): ('q0', 'x', 'R'),
+        ('q0', '0'): ('qa', '', 'R'),
     }
 )
 
